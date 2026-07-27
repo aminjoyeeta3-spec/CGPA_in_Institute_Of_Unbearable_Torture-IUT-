19626d4e74594256e58551084941f3d3cb60148c
Joyeeta___ <aminjoyeeta3@gmail.com>
2026-07-28 00:28:23 +0600

    again added make files

--- make.ps1
+++ make.ps1
@@ -1,5 +1,1 @@
-gcc -I modules tests/test_course.c modules/course.c -o test_course.exe
-.\test_course.exe
-
-gcc -I modules tests/test_course_result.c modules/course.c modules/courseResult.c -o test_course_result.exe
-.\test_course_result.exe
+gcc main.c -o calculator.exe
